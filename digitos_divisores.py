0a42abbb8bac45cff98be5c051e99fa9d330053a
FerYanez-jpg <fernandayc999@gmail.com>
2025-03-28 20:01:47 -0600

    Update digitos_divisores.py

--- digitos_divisores.py
+++ digitos_divisores.py
@@ -1,1 +1,11 @@
 # Codifica aquí tu programa
+usuario = int(input("Introducir enteros: "))
+
+divisor = 0
+
+for valor in str(usuario):
+    valor = int(valor)
+    if valor != 0 and usuario % valor == 0:
+        divisor += 1
+
+print(divisor)
